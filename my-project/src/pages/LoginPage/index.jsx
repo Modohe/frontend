04533bb75f45d96ec05f69960dbbc1ac26d137a0
heedda <heedda.kollim@gmail.com>
2025-05-20 13:49:02 +0900

    로그인 component 디자인 초기

--- my-project/src/pages/LoginPage/index.jsx
+++ my-project/src/pages/LoginPage/index.jsx
@@ -5,7 +5,9 @@ import Input from "@/components/LoginInput";
 export default function Login() {
   return (
     <S.LoginBackground>
-      <Input contents="로그인" type="password"/>
+      <S.LoginModal>
+        <Input contents="로그인" type="password" />
+      </S.LoginModal>
     </S.LoginBackground>
   );
 }
--- my-project/src/pages/LoginPage/styles.js
+++ my-project/src/pages/LoginPage/styles.js
@@ -1,5 +1,22 @@
 import styled from "styled-components";
 
 export const LoginBackground = styled.div`
-  
+  width: 100vw;
+  height: 100vh;
+  padding: 1vh 22vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginModal = styled.div`
+  width: 448px;
+  height: 656px;
+
+  box-shadow: 0px 8px 10px -6px rgba(0, 0, 0, 0.1),
+    0px 20px 25px -5px rgba(0, 0, 0, 0.1);
+  border-radius: 16px;
+
+  display: flex;
+  flex-direction: column;
 `;
